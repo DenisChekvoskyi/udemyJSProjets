f2a8c5c267ee7d930766462c9a0ae62e8e427cfb
Denis <chehovskiyd@gmail.com>
2022-08-31 19:14:33 +0300

    add send form to server

--- js/script.js
+++ js/script.js
@@ -40,11 +40,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const dedline = "2022-08-29";
 
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / (1000 * 60)) % 60),
-      seconds = Math.floor((t / 1000) % 60);
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      (days = Math.floor(t / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((t / (1000 * 60)) % 60)),
+        (seconds = Math.floor((t / 1000) % 60));
+    }
 
     return {
       total: t,
@@ -209,3 +218,54 @@ window.addEventListener("DOMContentLoaded", () => {
     ".menu .container"
   ).render();
 });
+
+// Forms
+const forms = document.querySelectorAll("form");
+
+const message = {
+  loading: "Загрузка",
+  success: "Спасибо, мы скоро с вами свяжемся",
+  failure: "Что-то пошло не так, повторите позже",
+};
+
+forms.forEach((item) => {
+  postData(item);
+});
+
+function postData(form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const statusMessage = document.createElement("div");
+    statusMessage.classList.add("status");
+    statusMessage.textContent = message.loading;
+    form.append(statusMessage);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "server.php");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    const formData = new FormData(form);
+
+    const object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+
+    const json = JSON.stringify(object);
+
+    request.send(json);
+
+    request.addEventListener("load", () => {
+      if (request.status === 200) {
+        console.log(request.response);
+        statusMessage.textContent = message.success;
+        form.reset();
+        setTimeout(() => {
+          statusMessage.remove();
+        }, 3000);
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+  });
+}
